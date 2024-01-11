4a5c9326112b6b3ac13cf54a8cac0fd17b84c31d
EMERY RODZIEWICZ <rod20012@byui.edu>
2024-01-10 22:06:08 -0500

    w02 prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNum = PromptUserNumber();
+        int squared = SquareNumber(userNum);
+
+        DisplayResult(userName, squared);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        return num;
+    }
+
+    static int SquareNumber(int userNum)
+    {
+        int sq = userNum * userNum;
+        return sq;
+    }
+
+    static void DisplayResult(string userName, double squared)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squared}.");
     }
 }
